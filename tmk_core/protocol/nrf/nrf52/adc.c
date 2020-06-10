22655721babbe559b0ed615948d30bcb373f3e09
tsiank <tsiank9@gmail.com>
2020-06-11 02:03:37 +0800

    Modify adc for my PCB

--- tmk_core/protocol/nrf/nrf52/adc.c
+++ tmk_core/protocol/nrf/nrf52/adc.c
@@ -46,12 +46,14 @@ void adc_init() {
 }
 
 void adc_start() {
-  nrf_saadc_channel_config_t pincfg = NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_VDD);
+  //nrf_saadc_channel_config_t pincfg = NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_VDD);
+  nrf_saadc_channel_config_t pincfg = NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN3); // tsiank
   nrf_drv_saadc_channel_init(0, &pincfg);
   nrf_drv_saadc_buffer_convert(adc_buffer, 1);
   nrf_drv_saadc_sample();
 }
 
 uint16_t get_vcc() {
-  return ((uint32_t)adc_buffer[0]*6*600/255);
+  //return ((uint32_t)adc_buffer[0]*6*600/255);
+  return ((uint32_t)adc_buffer[0]*6*600/255) * 4200 / 2456;
 }
